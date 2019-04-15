06ca6c8b1983017047cbddb3f8f0296716584b51
KendallCalderonR <Kendall-1132@hotmail.com>
2019-04-14 19:10:30 -0600

    prestamo completo
    error en $variable_prestamo = $_POST['prestamo'] despues de realizar el prestamo

--- Controladores/IngresarPrestamo.php
+++ Controladores/IngresarPrestamo.php
@@ -1,19 +1,20 @@
-<?php 
+<?php
+/* 
 require_once("../Modelos/conectar.php");
 require_once('../Modelos/libro_modelo.php');
-
-function InsertarPrestamo($id_prestamo, $fecha_prestamo, $fecha_entrega, $estado, $usuario_id, $id_libro){
-
+//$id_prestamo,
+function InsertarPrestamo( $fecha_prestamo, $fecha_entrega, $estado, $usuario_id, $id_libro){
+//$id_prestamo,
 	$objeto = new libro_modelo();
-	$objeto->PrestarLibro($id_prestamo,$fecha_prestamo,$fecha_entrega,$estado,$usuario_id,$id_libro);
+	$objeto->PrestarLibro($fecha_prestamo,$fecha_entrega,$estado,$usuario_id,$id_libro);
 }
 
 //$datos = $prestamo;
-function EjecutaInsertar(){
+/*function EjecutaInsertar(){
 	//jalamos las variables del libro que son SESSION	
 	///meterlo asi
 
-	$p_id_prestamo=100;
+	//$p_id_prestamo=100;
 	$_SESSION["cantidad"];
 	$_SESSION["nuevaCantidad"];
 	$p_id_libro = $_SESSION["id_libro"];
@@ -21,28 +22,90 @@ function EjecutaInsertar(){
 	$p_fecha_entrega = $_SESSION["fecha_entrega"];
 	$p_estado = $_SESSION["estado"];
 	$p_usuario_id = $_SESSION["usuario_id"];
+	//$p_id_prestamo, 
+	InsertarPrestamo($p_fecha_prestamo, $p_fecha_entrega, $p_estado,$p_usuario_id,$p_id_libro);
 
-	InsertarPrestamo($p_id_prestamo, $p_fecha_prestamo, $p_fecha_entrega, $p_estado,$p_usuario_id,$p_id_libro);
+	echo "Prestamo Ingresado";
+}
+*/
+/*
+function EjecutaInsertar(){
+
+	$_SESSION["cantidad"];
+	$_SESSION["nuevaCantidad"];
+	$p_id_libro = $_SESSION["id_libro"];
+	$p_fecha_prestamo = $_SESSION["fecha_prestamo"];
+	$p_fecha_entrega = $_SESSION["fecha_entrega"];
+	$p_estado = $_SESSION["estado"];
+	$p_usuario_id = $_SESSION["usuario_id"];
+	//$p_id_prestamo, 
+	InsertarPrestamo($p_fecha_prestamo, $p_fecha_entrega, $p_estado,$p_usuario_id,$p_id_libro);
 
 	echo "Prestamo Ingresado";
 }
 
-/*function EjecutaInsertar(){
-	$mensaje=$_SESSION["cantidad"];
-	echo "$mensaje";
-}*/
+/*function LimpiarVariables(){
+
+		$_SESSION["cantidad"] = "";
+		$_SESSION["nuevaCantidad"]= "";
+		$_SESSION["id_libro"]= "";
+		$_SESSION["fecha_prestamo"]= "";
+		$_SESSION["fecha_entrega"]= "";
+		$_SESSION["estado"]= "";
+	}*/
 
  ?>
 
-
-<script type="text/javascript">
+<?php 
+/*<script type="text/javascript">
 	function CargarPrestamo(){
 		//var mensaje = '<?php;?>';
 		//alert(mensaje);
-		var mensaje="<?php EjecutaInsertar();?>";// ASI SE LLAMA A LA FUNCION
-		alert(mensaje);
+		//var mensaje="<?php// EjecutaInsertar();?>";// ASI SE LLAMA A LA FUNCION
+		//alert(mensaje);
+
+		<?php EjecutaInsertar(); ?>
+		alert("Prestamo procesado");
+		//<?php //LimpiarVariables(); ?>
+
 	}
-</script>
+</script>*/
+ ?>
+
+
+
+<?php 
+//Nuevo php 14/04/19 17:53pm
+
+
+
+require_once('../Modelos/libro_modelo.php');
+$libro = new libro_modelo();
+
+   /*valido que se haya enviado el formulario para crear el objeto y llamar el metodo PrestarLibro() para insertar el registro*/
+    if(isset($_POST["enviar"]) and $_POST["enviar"]=="si"){
+ 
+        /*declaro las variable*/    
+        $fecha_prestamo=$_POST["fecha_prestamo"];
+        $fecha_entrega=$_POST["fecha_entrega"];
+        $estado=$_POST["estado"];
+        $id_usuario=$_POST["id_usuario"];
+        $id_libro=$_POST["id_libro"];
+
+        
+      $libro->PrestarLibro($fecha_prestamo,$fecha_entrega,$estado,$id_usuario,$id_libro);
+        
+        echo "<script type='text/javascript'>
+		alert('Prestamo exitoso');
+			</script>";
+      
+    }//cierre del if de validacion de envio de formulario
+
+
+    
+
+
+ ?>
 
 
 
--- Vistas/InfoLibro.php
+++ Vistas/InfoLibro.php
@@ -1,6 +1,10 @@
 <?php 
 
+//if (isset($_POST['prestamo'])) {
+
 $variable_prestamo = $_POST['prestamo'] ;
+
+
 
 require_once('../Vistas/header.php');
 require_once('../Modelos/libro_modelo.php');
@@ -33,17 +37,26 @@ $usuarios = $usu->ObtenerUsuarioPorUsuario($dato);
  foreach ($libros as $libro) {
 
  	//Variables del libro para prestamo
-$_SESSION["cantidad"]=$libro["cantidad"];
+/*$_SESSION["cantidad"]=$libro["cantidad"];
 $_SESSION["nuevaCantidad"]= $_SESSION["cantidad"]-1;
 $_SESSION["id_libro"] = $libro["id_libro"];
 $_SESSION["fecha_prestamo"]= date("y-m-d");
 $_SESSION["fecha_entrega"]= date("y-m-d",strtotime('+7 day',strtotime($_SESSION["fecha_prestamo"])));
 $_SESSION["estado"] = 1;
 $_SESSION["usuario_id"] = $usuario[0];
+*/
+
+$cantidad=$libro["cantidad"];
+$nuevaCantidad= $cantidad-1;
+$id_libro = $libro["id_libro"];
+$fecha_prestamo= date("y-m-d");
+$fecha_entrega= date("y-m-d",strtotime('+7 day',strtotime($fecha_prestamo)));
+$estado = 1;
+$usuario_id = $usuario[0];
 
 //-------------------------------------------------------------------------------------
  	
-echo "<p class=\"id_libro\" >". $libro["id_libro"]."</p>";
+/*echo "<p class=\"id_libro\" >". $libro["id_libro"]."</p>";
 echo '<br>';
 echo "<p class=\"autor_libro\" >" .$libro["autor"]."</p>";
 echo '<br>';
@@ -55,15 +68,47 @@ echo "<p class=\"resena_libro\" width=\"250\">".$libro["resena"]."</p>";
 echo '<br>';
 echo "<img src= ../Public/imagenes/". $libro["imagen"]." width=\"150\" height=\"170\" >";
 echo '<br>';
-echo "<input class=\"boton_libro\" name=\"prestamo\" type=\"button\" value=\"Prestamo\" onclick=\"CargarPrestamo();\" />";
+//echo "<input class=\"boton_libro\" name=\"prestamo\" type=\"button\" value=\"Prestamo\" onclick=\"CargarPrestamo();\" />";
 echo "<span id='php_code'></span>";
  }
 echo '<br>';
+*/	
 
 
 ?>
+<form id="InfoLibro" method="post">
+	
+	<p class="id_libro" id="numerolibro" name="numerolibro"><?php echo $libro["id_libro"]?></p><br>
+	<p class="autor_libro" id="autor_libro" name="autor_libro" ><?php echo $libro["autor"] ?></p><br>
+	<p class="titulo_libro" id="titulo_libro" name="titulo_libro" ><?php echo $libro["titulo"] ?></p><br>
+	<p class="cantidad_libro" id="cantidad_libro" name="cantidad_libro" ><?php echo $libro["cantidad"] ?></p><br>
+	<p class="resena_libro" id="resena_libro" name="resena_libro" width="250"><?php echo $libro["resena"] ?></p><br>
+	<img src= <?php echo"../Public/imagenes/".$libro["imagen"]." width=\"150\" height=\"170\" ";?> ><br>
+	<input type="hidden" name="id_libro" id="id_libro" value=<?php echo "\"".$id_libro."\""; ?>>
+	<input type="hidden" name="fecha_prestamo" id="fecha_prestamo" value=<?php echo "\"".$fecha_prestamo."\""; ?>>
+	<input type="hidden" name="fecha_entrega" id="fecha_entrega" value=<?php echo "\"".$fecha_entrega."\""; ?>>
+	<input type="hidden" name="estado" id="estado" value=<?php echo "\"".$estado."\""; ?>>
+	<input type="hidden" name="id_usuario" id="id_usuario" value=<?php echo "\"".$usuario_id."\""; ?>>
+	<input type="hidden" name="nuevaCantidad" id="nuevaCantidad" value=<?php echo "\"".$nuevaCantidad."\""; ?>>
+	<input type="hidden" name="enviar" value="si">
+    <input class="boton_libro" name="submit" type="submit" value="Prestamo"/>
+</form>
+
+<?php } ?>
+
 <a href="../Vistas/libros_vista.php" class="">Libros</a>
+
+
+
 
 <?php 
 require_once('../Vistas/Footer.php');
+/*}else {
+	echo "<script type='text/javascript'>
+	window.location='../Vistas/libros_vista.php';
+		</script>";
+
+		//header('../Vistas/libros_vista.php');
+
+}*/
  ?>
