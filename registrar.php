d1205a814372493a0797615ddf40797a87096504
Fran Perez <franpc_@hotmail.com>
2019-04-20 12:16:31 -0600

    Validaciones
    Se realizan las validaciones en javascript.

--- registrar.php
+++ registrar.php
@@ -4,7 +4,8 @@ require_once('Modelos/conectar.php');
 //require_once("Modelos/Usuarios_modelo.php"); 
 echo '<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 	  <script src="jquery-validate/dist/jquery.validate.min.js"></script>
-      <script type="text/javascript" src="js/scripts.js"></script>'
+      <script type="text/javascript" src="js/scripts.js"></script>
+      <script type="text/javascript" src="Public/js/validaciones.js"></script>'
 
  ?>
 
@@ -19,32 +20,32 @@ echo '<script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 
 		<div class="form-group">
 			<label for="nombre"> Nombre:</label>
-			<input type="text" name="nombre" pattern="^([A-Z]{1}[a-zñáéíóú]+[\s]*)+$" title="Empiece con Mayúscula y no utilice números" class="form-control" id="nombre" placeholder="Ingrese su nombre" required="">
+			<input type="text" name="nombre"  title="Empiece con Mayúscula y no utilice números" class="form-control" id="nombre" placeholder="Ingrese su nombre" >
 		</div>
 
 		<div class="form-group">
 			<label for="apellido"> Apellido:</label>
-			<input type="text" name="apellido" pattern="^([A-Z]{1}[a-zñáéíóú]+[\s]*)+$" title="Empiece con Mayúscula y no utilice números" class="form-control" id="apellido" placeholder="Ingrese su apellido" required="">
+			<input type="text" name="apellido" title="Empiece con Mayúscula y no utilice números" class="form-control" id="apellido" placeholder="Ingrese su apellido" >
 		</div>
 		<div class="form-group">
 			<label for="cedula"> Cédula:</label>
-			<input type="text" name="cedula" pattern="^[1-9]\d{4}\d{4}$" title="Cumpla con formato: 1 1111 1111" class="form-control" id="cedula" placeholder="Ingrese su numero de cédula sin guiones" required="">
+			<input type="text" name="cedula" title="Cumpla con formato: 1 1111 1111" class="form-control" id="cedula" placeholder="Ingrese su numero de cédula sin guiones" >
 		</div>
 		<div class="form-group">
 			<label for="email"> Email:</label>
-			<input type="text" name="email" pattern="^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$" title="Cumpla con formato: ejemplo@example.com" class="form-control" id="email" placeholder="Ingrese su Email" required="">
+			<input type="text" name="email"  title="Cumpla con formato: ejemplo@example.com" class="form-control" id="email" placeholder="Ingrese su Email" >
 		</div>
 		<div class="form-group">
 			<label for="usuario"> Usuario:</label>
-			<input type="text" name="usuario" pattern="^[a-z0-9ü_-][a-z0-9ü_-]{3,9}$" title="En minúscula, no utilice tildes, máximo 10 caracteres" class="form-control" id="usuario" placeholder="Ingrese su usuario" required="">
+			<input type="text" name="usuario"  title="En minúscula, no utilice tildes, máximo 10 caracteres" class="form-control" id="usuario" placeholder="Ingrese su usuario" >
 		</div>
 		<div class="form-group">
 			<label for="contrasena"> Contraseña:</label>
-			<input type="password" name="contrasena" class="form-control" id="contrasena" placeholder="Ingrese su contraseña" required="">
+			<input type="password" name="contrasena" class="form-control" id="contrasena" placeholder="Ingrese su contraseña" >
 		</div>
 
 		<div class="form-group">
-			<button type="submit" name="enviar" value="si"  class="btn btn-dark">Crear Usuario</button>
+			<button type="button" name="enviar" value="si" class="btn btn-dark">Crear Usuario</button>
 		</div>
 	</form>
 </div>
@@ -56,7 +57,8 @@ include('Modelos/Usuarios_modelo.php');
 
 
 if (isset($_POST["nombre"],$_POST["apellido"],$_POST["cedula"],
-		  $_POST["email"],$_POST["usuario"],$_POST["contrasena"]) 
+		  $_POST["email"],$_POST["usuario"],$_POST["contrasena"])
+
 	     /* and (
 		  $_POST["nombre"]!="",$_POST["apellido"]!="",$_POST["cedula"]!="",
 		  $_POST["email"]!="",$_POST["usuario"]!="",$_POST["contrasena"]!="")*/) {
